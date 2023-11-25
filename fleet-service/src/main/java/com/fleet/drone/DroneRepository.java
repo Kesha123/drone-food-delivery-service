2df22b7cefda0526072934bcc5ad7f1b9c66cd0f
Innokentii Kozlov <innokentiikozlov@gmail.com>
2023-11-25 21:04:19 +0200

    Create food order

--- fleet-service/src/main/java/com/fleet/drone/DroneRepository.java
+++ fleet-service/src/main/java/com/fleet/drone/DroneRepository.java
@@ -3,9 +3,10 @@ package com.fleet.drone;
 import com.fleet.drone.Drone;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DroneRepository extends MongoRepository<Drone, String> {
     Optional<Drone> findByNickname(String nickname);
-    Optional<Drone> findByAvailable(Boolean available);
+    Optional<List<Drone>> findByAvailable(Boolean available);
 }
